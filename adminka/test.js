ffefdb605989c286efb15b4088c20505ff89651e
ArtemSolonyi <93489903+ArtemSolonyi@users.noreply.github.com>
2024-05-16 16:35:31 +0300

    Delete adminka directory

--- adminka/test.js
+++
@@ -1,5 +0,0 @@
-function extractNumbersFromString(str) {
-  const numbersOnly = str.replace(/\D/g, '');
-  return numbersOnly;
-}
-console.log(extractNumbersFromString('1213dfdfd232'),'extractNumbersFromString')
